88f070016cf7f60f0a11b665171cf778d2dae2dc
lluidesia <liudaprysiazhna@gmail.com>
2016-05-31 22:43:16 +0300

    elementary 11 end of other

---
+++ Elementary/11-end-of-other.py
@@ -0,0 +1,15 @@
+def checkio(words_set):
+    for word in words_set:
+        for suf in words_set-{word}:
+            if word.endswith(suf):
+                return True
+    return False
+    
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
